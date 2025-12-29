0c810ac6d08f2f5c3b29ca4938d6357cd07b94eb
Biku4321 <samantabikash83939@gmail.com>
2025-12-29 22:26:52 +0530

    Update project stucture and files

--- client/src/router/AppRoutes.jsx
+++ client/src/router/AppRoutes.jsx
@@ -1,26 +1,3 @@
-// import React from 'react';
-// import { Routes, Route } from 'react-router-dom';
-// import Home from '../pages/Home';
-// import About from '../pages/About';
-// import Skills from '../pages/Skills';
-// import Projects from '../pages/Projects';
-// import Contact from '../pages/Contact';
-// // Add other pages here
-
-// const AppRoutes = () => {
-//   return (
-//     <Routes>
-//       <Route path="/" element={<Home />} />
-//       <Route path="/about" element={<About />} />
-//       <Route path="/skills" element={<Skills />} />
-//       <Route path="/projects" element={<Projects />} />
-//       <Route path="/contact" element={<Contact />} />
-//     </Routes>
-//   );
-// };
-
-// export default AppRoutes;
-
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
