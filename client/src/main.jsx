0c132cdc766fd2c055c958dddfb927c9557d3fa0
Biku4321 <samantabikash83939@gmail.com>
2026-04-28 12:19:06 +0530

    add vercel analytics

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,8 @@ import App from "./App";
 import "./index.css";
 import { ToastProvider } from "./context/ToastContext";
 import { HelmetProvider } from "react-helmet-async";
+import { inject } from '@vercel/analytics'
+inject()
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ToastProvider>
